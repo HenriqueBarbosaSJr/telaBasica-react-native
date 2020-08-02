e34267ad216cd2cd7e52f608e2daf69c18eb4827
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-08-02 20:17:13 -0300

    Ponto inicial - Projeto Tela Básica

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste de Applicativos</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.textInput}
+        placeholder='Entre com seu nome'>
+      </TextInput>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}>Clique Aqui !</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +21,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput:{
+    backgroundColor:'#FFF',
+    width:'80%',
+    marginBottom:20,
+    padding:10,
+    color:'#222',
+    fontSize:17,
+    borderRadius:7,
+
+  },
+  button: {
+    backgroundColor: '#35AAFF',
+    width:'80%',
+    height:45,
+    borderRadius:7,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  textButton: {
+    color:'#ffff'
+  }
 });
---
+++ README.md
@@ -0,0 +1,23 @@
+# Tela básica de um aplicativo Mobile
+
+Exemplo de tela básica de um aplicativo Mobile utilizando React Native.
+
+ 
+ 
+  Foi utilizado o EXPO para a criação do projeto visando a facilidade de testes em dispositivo real tanto em IOS quando Android.
+
+ 
+ 
+
+ ### 📒 Languages
+
+  * React
+  * React Native
+  * Javascript
+
+ ### 📡 Technologies
+ * EXPO 
+ 
+
+  ---
+ Made by 💙 by [Henrique Barbosa de Souza Júnior](https://github.com/HenriqueBarbosaSJr)
